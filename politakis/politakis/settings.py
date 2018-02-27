e1b56847c107c589b4ea9a95d7f1b296493968e5
Renjigr <renji_gr@hotmail.com>
2018-02-28 01:19:21 +0200

    Update settings.py

--- politakis/politakis/settings.py
+++ politakis/politakis/settings.py
@@ -22,7 +22,6 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'work/templates')
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = '4=s&(fwz7gp(od%=^^ey#7mnuj_ry1)nywtm9^+8(xp0sn5#b$'
 import os
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
